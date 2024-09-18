329ca9203ff9ef0d4be41f3f95e53def109d54d5
Lalla Sankara <lallasankara@gmail.com>
2024-09-17 22:27:27 -0700

    day 2 code updates

---
+++ day-01/Student.py
@@ -0,0 +1,8 @@
+## Class deifning what student is - template
+class Student:
+
+    def _init_(self, name, major, gpa, is_on_probabtion):
+        self.name = name
+        self.major = major
+        self.gpa = gpa
+        self.is_on_probation = is_on_probabtion
---
+++ day-01/writingfiles.py
@@ -0,0 +1,5 @@
+employee_file = open("index.html", "a")
+
+employee_file.write("<p>This is HTML</p>")
+
+employee_file.close()
